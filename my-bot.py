b5322fe9c3429bef5d183b1a6c55713a62ffc35a
ryanlion1549 <72112664+ryanlion1549@users.noreply.github.com>
2020-09-30 11:40:31 +0900

    Update my-bot.py

--- my-bot.py
+++ my-bot.py
@@ -24,9 +24,7 @@ async def on_message(message):
     if message.content.startswith("!시간표1-2"):
         await message.channel.send("https://media.discordapp.net/attachments/625652526028554262/760355342873395250/unknown.png")
     if message.content.startswith("!Q"):
-        embed = discord.Embed(title=":school: 원하시는 반의 시간표 명령어를 입력하세요.", description="현재 없는 반은 추후 추가 예정입니다.", color=0xfcff00)
-        embed.add_field(name="신흥고등학교 1학년 1,2,4반", value="`!시간표1-1`  `!시간표1-2`  `!시간표1-4`", inline=False)
-        await message.channel.send(embed=embed)
+       await message.channel.send("!시간표(학년-반) ex)!시간표1-1")
     if message.content.startswith("준석이한테 지랄해줘"):
         await message.channel.send("그럴 순 없어요")
     if message.content.startswith("상현이한테 지랄해줘"):
